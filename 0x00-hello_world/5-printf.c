2b0fb00557225991397740a0734f021a153c752d
PantheSpace <dammyadesola@gmail.com>
2023-02-18 03:37:25 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ *
+ *
+ * print a string and return 0 then exits
+ * Return: int (0)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
