a215da340b42173cb9023fdac97fb49d20802be1
PantheSpace <dammyadesola@gmail.com>
2023-02-19 09:31:50 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,26 @@
-#include <stdio.h>
-/**
-* main - main function entry
-*
-* Return: int(0)
-*/
 int main(void)
 {
-	int i, j, k, l;
+    int i, j;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 9; j++)
-		{
-			for (k =  0; k < 10; k++)
-			{
-				for (l = 1; l < 10; l++)
-				{
-					putchar((char) (i + 48));
-					putchar((char) (j + 48));
-					putchar(' ');
-					putchar((char) (k + 48));
-					putchar((char) (l + 48));
-					if (i == 9 && j == 8 && k == 9 && l == 9)
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+    for (i = 0; i < 99; i++)
+    {
+        for (j = i + 1; j <= 99; j++)
+        {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+
+            if (i != 98 || j != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
 }
