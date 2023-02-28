dfb34b4dac7e65b6e52606da6f82bccf0cbe0827
PantheSpace <dammyadesola@gmail.com>
2023-02-28 06:37:01 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first
+ * @str: The string to print
+ */
+void puts2(char *str)
+{
+    int i = 0;
+
+    while (*(str + i) != '\0')
+    {
+        if (i % 2 == 0)
+        {
+           putchar(*(str + i));
+        }
+        i++;
+    }
+
+    putchar('\n');
+}
