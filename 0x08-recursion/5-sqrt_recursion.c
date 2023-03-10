c2be56d58b705c02031625c03cb87b3268ef7018
PantheSpace <dammyadesola@gmail.com>
2023-03-10 15:16:55 -0800

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,11 @@
  */
 int sqrt_helper(int n, int i)
 {
-    if (i * i > n)
-        return (-1);
-    if (i * i == n)
-        return (i);
-    return (sqrt_helper(n, i + 1));
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (sqrt_helper(n, i + 1));
 }
 
 /**
@@ -25,7 +25,7 @@ int sqrt_helper(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    return (sqrt_helper(n, 0));
+if (n < 0)
+return (-1);
+return (sqrt_helper(n, 0));
 }
