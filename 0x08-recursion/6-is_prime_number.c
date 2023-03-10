fc5b3fb495e2114d1dc9837f3b38a1b58f260845
PantheSpace <dammyadesola@gmail.com>
2023-03-10 15:28:58 -0800

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,24 +2,24 @@
 #include "main.h"
 
 /**
- * is_prime_number - checks if a number is prime recursively
- * @n: the number to check
+ * is_prime_number - checks if a number is prime
+ * @n: input number
  *
- * Return: 1 if the number is prime, 0 otherwise
+ * Return: 1 if n is prime, 0 otherwise
  */
+
 int is_prime_number(int n)
 {
-/* Base cases */
+int i;
+
 if (n <= 1)
 return (0);
-if (n <= 3)
-return (1);
 
-/* Check if n is divisible by any number up to sqrt(n) */
-int i;
-for (i = 2; i * i <= n; i++)
+for (i = 2; i < n; i++)
+{
 if (n % i == 0)
 return (0);
+}
 
 return (1);
 }
