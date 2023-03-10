0a540f3e373bd92a6e67dfd8926161fa0eace7dc
PantheSpace <dammyadesola@gmail.com>
2023-03-10 13:46:34 -0800

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,15 @@
  * _puts_recursion - prints string to the output
  * @str: array of chars
  * Return: nothing
-*/
+ */
 
-void _puts_recursion(char *str) 
+void _puts_recursion(char *str)
 {
-if (*str == '\0') 
- {
-    putchar('\n');
-    return;
- }
-putchar(*str);
-_puts_recursion(str + 1);
+    if (*str == '\0')
+    {
+        putchar('\n');
+        return;
+    }
+    putchar(*str);
+    _puts_recursion(str + 1);
 }
