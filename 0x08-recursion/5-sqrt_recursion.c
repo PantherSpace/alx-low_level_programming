b50402553f16789ec56c80037df2853d1ad080d5
PantheSpace <dammyadesola@gmail.com>
2023-03-10 15:14:02 -0800

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,18 +2,30 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * sqrt_helper - recursively calculate the square root of a number
+ * @n: the number to calculate the square root of
+ * @i: the current guess for the square root
  *
- * @n: number to calculate the square root of
+ * Return: the square root of n if it is a perfect square, otherwise -1
+ */
+int sqrt_helper(int n, int i)
+{
+    if (i * i > n)
+        return (-1);
+    if (i * i == n)
+        return (i);
+    return (sqrt_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - calculate the natural square root of a number
+ * @n: the number to calculate the square root of
  *
- * Return: natural square root of `n`, or -1 if `n` does not have a natural
- *         square root
+ * Return: the square root of n if it is a perfect square, otherwise -1
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-if (n == 0 || n == 1)
-return (n);
-return (sqrt_helper(n, 1, n));
+    if (n < 0)
+        return (-1);
+    return (sqrt_helper(n, 0));
 }
